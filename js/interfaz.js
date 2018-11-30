4f3bac50c1af465dc169113631d5a8f7d82ae90a
D4ITON <daitonmh100@gmail.com>
2018-11-30 08:16:18 -0500

    estilos de menu izquierdo

--- js/interfaz.js
+++ js/interfaz.js
@@ -1,33 +1,60 @@
 /*
 	SCRIPTS PARA LA INTERFAZ
 */
-console.log('interfaz.js mounted');
 
-var activado = false; // significa el estado de la herramienta
-var $funcionId = $("#funcion_01");
-
-$funcionId.on('click', function(e){
-console.log('clicked');
-	$(this).toggleClass("active");
-	e.preventDefault();
-});
+// $funcionId.on('click', function(e){
+// 	$(this).toggleClass("active");
+// 	e.preventDefault();
+// });
 
 /*  */
 
 // variables
-  var mouse = document.getElementById('funcion_01');
-  var trazo_aspero = document.getElementById('funcion_02');
-  var trazo_fino = document.getElementById('funcion_03');
-  var aerografo = document.getElementById('funcion_04');
-  var texto = document.getElementById('funcion_05');
-  var linea = document.getElementById('funcion_06');
-  var circulo = document.getElementById('funcion_07');
-  var guardar = document.getElementById('funcion_08');
-  var rectangulo = document.getElementById('funcion_09');
-  var limpiar_scren = document.getElementById('funcion_10');
+var mouse = $("#funcion_01");
+var trazo_aspero = $("#funcion_02");
+var trazo_fino = $("#funcion_03");
+var aerografo = $("#funcion_04");
+var texto = $("#funcion_05");
+var linea = $("#funcion_06");
+var circulo = $("#funcion_07");
+var guardar = $("#funcion_08");
+var rectangulo = $("#funcion_09");
+var limpiar_scren = $("#funcion_10");
 
 
+// console.log(mouse);
+// var operacion;
+
+$(function (){
+	$("#menu-left button").on("click", function(){
+		$("button.active").removeClass('active');
+		$(this).addClass('active');
+	});
+});
+
+// $( function() {
+//     $( "#button" ).on( "click", function() {
+//       $( "#effect" ).toggleClass( "newClass", 1000 );
+//     });
+//   } );
 
 
+// switch (operacion) {
+//   case 01:
+    
+//     break;
+//   default:
+//     // statements_def
+//     break;
+// }
 
+// function active(id)
+// {
+//   console.log(id);
+// }
 
+// $funcionId.on('click', function(e){
+// console.log('clicked');
+//   $(this).toggleClass("active");
+//   e.preventDefault();
+// });

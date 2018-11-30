066cc3d441696e125681ff55c3bd199e66ade038
D4ITON <daitonmh100@gmail.com>
2018-11-30 11:50:38 -0500

    agregando dibujar areografo en menu

--- js/app.js
+++ js/app.js
@@ -7,22 +7,49 @@
   @curso    : CAD
 */
 
-/* ------ DETECCION DE LA POSICION DEL MOUSE------  */
+/* ============= DIBUJAR A MANO ALZADA ============= */
 
-var limpiar = document.getElementById("funcion_10");
+/* ------ DETECCION DE LA POSICION DEL MOUSE------  */
+/* obteniendo el canvas o espacio de trabajo */
+var limpiar = document.getElementById("funcion_09");
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var cw = canvas.width = 950,
+var cw = canvas.width = 850,
   cx = cw / 2;
 var ch = canvas.height = 500,
   cy = ch / 2;
 
-var dibujar = false;
-var factorDeAlisamiento = 5;
-var Trazados = [];
-var puntos = [];
-ctx.lineJoin = "round";
+/* ===== deteccion de la posicion del mouse ===== */
 
+function oMousePos(canvas, evt) {
+  var ClientRect = canvas.getBoundingClientRect();
+  return { //objeto
+    x: Math.round(evt.clientX - ClientRect.left),
+    y: Math.round(evt.clientY - ClientRect.top)
+  }
+}
+
+/* =========== MENU DE OPCIONES =========== */
+$(function(){
+    $("#menu-left button").on("click", function(){
+      // dibujar_trazo_fino();
+      // dibujar_areografo();
+    });
+});
+
+/* ========== FUNCION TRAZO FINO ========== */
+/* ======================================== */
+
+function dibujar_trazo_fino() {
+
+  // variables
+  var dibujar = false;
+  var factorDeAlisamiento = 5;
+  var Trazados = [];
+  var puntos = [];
+  ctx.lineJoin = "round";
+
+// funcion limpiar pantalla
 limpiar.addEventListener('click', function(evt) {
   dibujar = false;
   ctx.clearRect(0, 0, cw, ch);
@@ -55,9 +82,6 @@ canvas.addEventListener("mousemove", function(evt) {
     ctx.stroke();
   }
 }, false);
-
-
-
 
 
 function reducirArray(n,elArray) {
@@ -99,16 +123,78 @@ function redibujarTrazados(){
   ctx.clearRect(0, 0, cw, ch);
   reducirArray(factorDeAlisamiento,puntos);
   for(var i = 0; i < Trazados.length; i++)
-  alisarTrazado(Trazados[i]);
+    alisarTrazado(Trazados[i]);
 }
-/* ------ deteccion de la posicion del mouse ------ */
 
-function oMousePos(canvas, evt) {
-  var ClientRect = canvas.getBoundingClientRect();
-  return { //objeto
-    x: Math.round(evt.clientX - ClientRect.left),
-    y: Math.round(evt.clientY - ClientRect.top)
+}
+
+/* ========== FUNCION AREOGRAFO ========== */
+/* ======================================== */
+
+function dibujar_areografo() {
+  
+
+var dibujar = false;
+
+var densidad_aerografo = 50;
+var radio_aerografo = 10;
+//var color_aerografo = "rgb(106,177,80)";
+
+//crea un rectángulo blanco del tamaño del canvas
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, cw, ch);
+
+var imgData = ctx.getImageData(0, 0, cw, ch);
+var pixels = imgData.data;
+
+
+//eventos
+canvas.addEventListener('mousedown', function(evt) {
+  dibujar = true;
+}, false);
+
+canvas.addEventListener('mouseup', function(evt) {
+  dibujar = false;
+}, false);
+
+canvas.addEventListener("mouseout", function(evt) {
+  dibujar = false;
+}, false);
+
+canvas.addEventListener("mousemove", function(evt) {
+  if (dibujar) {
+    var m = oMousePos(canvas, evt);
+    Aerografo(m);
   }
+}, false);
+
+limpiar.addEventListener('click', function() {
+  ctx.fillRect(0,0,cw,ch);
+  imgData = ctx.getImageData(0, 0, cw, ch);
+  pixels = imgData.data;
+}, false);
+
+
+
+function Aerografo(m) {
+
+  for (var p = 0; p < densidad_aerografo; p++) {
+    
+    var a = Math.random() * Math.PI * 2;//el ángulo
+    var r = Math.random() * radio_aerografo;// el radio
+    var x = ~~(m.x + r * Math.cos(a));
+    var y = ~~(m.y + r * Math.sin(a));
+    var i = (x + y * imgData.width) * 4; //el index
+
+    pixels[i + 0] = 106; //rojo
+    pixels[i + 1] = 177; //verde
+    pixels[i + 2] = 80; //azul
+
+  }
+
+  ctx.putImageData(imgData, 0, 0);
+
 }
 
-/* ------ fin de detectar la posicion del mouse ------ */
+}
--- js/interfaz.js
+++ js/interfaz.js
@@ -1,13 +1,11 @@
 /*
-	SCRIPTS PARA LA INTERFAZ
+	@título   : Interfaz basada en "Adobe XD CC"
+	@fecha    : 26.11.2018
+	@tipo 	  : JavaScript
+	@autor    : DALTHON
+	@versión  : 0.0.5
+	@curso    : CAD
 */
-
-// $funcionId.on('click', function(e){
-// 	$(this).toggleClass("active");
-// 	e.preventDefault();
-// });
-
-/*  */
 
 // variables
 var mouse = $("#funcion_01");
@@ -17,44 +15,53 @@ var aerografo = $("#funcion_04");
 var texto = $("#funcion_05");
 var linea = $("#funcion_06");
 var circulo = $("#funcion_07");
-var guardar = $("#funcion_08");
-var rectangulo = $("#funcion_09");
-var limpiar_scren = $("#funcion_10");
+// var guardar = $("#funcion_08");
+var rectangulo = $("#funcion_08");
+var limpiar_scren = $("#funcion_09");
 
-
-// console.log(mouse);
-// var operacion;
+var limpiar_scren_id = limpiar_scren.attr("id");
+/* AGREGAR CLASE ACTIVE A LA FUNCION SELECCIONADA  */
+var ultimo_seleccionado_id; // centinela que se usa para guardar el ultimo evento usado
+var ultimo_seleccionado; // captura la ultima seleccion
+var seleccionadas = []; // array que registra las funciones usadas
 
 $(function (){
 	$("#menu-left button").on("click", function(){
+		
+		ultimo_seleccionado = $(this);
+		ultimo_seleccionado_id = $(this).attr("id");
+		seleccionadas.push(ultimo_seleccionado);
+		var delayInMilliseconds = 500; 
+		console.log($(this).attr("id")); //retorna la funcion con su id
+
+		/* QUITA LA CLASE A LOS OTRAS FUNCIONES Y SE QUEDA CON EL EL ULTIMO */
 		$("button.active").removeClass('active');
 		$(this).addClass('active');
+
+
+		/* REGRESAR A LA PRIMERA FUNCIION LUEGO DE LIMPIAR PANTALLA  */
+		/* HACER UN DELAY EN LA FUNCION */
+		setTimeout(function(){
+
+			if (ultimo_seleccionado_id==limpiar_scren_id) 
+			{
+				// console.log(seleccionadas[seleccionadas.length - 1]);
+				// var penultimo_seleccionado = seleccionadas[seleccionadas.length - 2];
+				$("button.active").removeClass('active');
+				mouse.addClass('active');
+			}
+
+		}, delayInMilliseconds);
+
 	});
 });
 
-// $( function() {
-//     $( "#button" ).on( "click", function() {
-//       $( "#effect" ).toggleClass( "newClass", 1000 );
-//     });
-//   } );
 
 
-// switch (operacion) {
-//   case 01:
-    
-//     break;
-//   default:
-//     // statements_def
-//     break;
-// }
+/* DESCARGAR TODO LO QUE HAY EN EL CANVAS COMO IMAGEN */
+var download = document.getElementById("download");
 
-// function active(id)
-// {
-//   console.log(id);
-// }
-
-// $funcionId.on('click', function(e){
-// console.log('clicked');
-//   $(this).toggleClass("active");
-//   e.preventDefault();
-// });
+download.addEventListener("click", function() {
+  var image = document.getElementById("canvas").toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+  download.setAttribute("href", image);
+});
